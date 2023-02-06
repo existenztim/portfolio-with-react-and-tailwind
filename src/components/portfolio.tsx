65b06d980edca4089334414267f6d32d0af48deb
existenztim <tim920129@hotmail.com>
2023-02-06 18:23:22 +0100

    :tada: Beta version ready!

--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -10,42 +10,42 @@ interface portfolio {
 }
 const portfolioData:portfolio[] = [
     {
-        imgLink: "/img/quant.jpg",
+        imgLink: "/img/quant.webp",
         alt: "Desktop screenshot of firefly project",
         title: "Quant Firefly",
-        details: "Javascript HTML CSS.",
-        demoLink: "https://existenztim.github.io/Cluedo-with-localstorage-highscore/",
+        details: "JavaScript, HTML, SCSS, GSAP, Prettier, ESLint.",
+        demoLink: "https://existenztim.github.io/Graphical-design-tools-coding-a-design/",
         id: "1"
     },
     {
-        imgLink: "/img/gottfrid.jpg",
+        imgLink: "/img/gottfrid.webp",
         alt: "Desktop screenshot of gottfrid donuts project",
         title: "Gottfrids Donutshop",
-        details: "Javascript HTML CSS",
-        demoLink: "https://existenztim.github.io/Cluedo-with-localstorage-highscore/",
+        details: "JavaScript, HTML, SCSS, GSAP, Prettier, ESLint.",
+        demoLink: "https://medieinstitutet.github.io/fed22d-js-grundkurs-gruppuppgift-webbshop-team-n00bless/",
         id: "2"
     },
     {
-        imgLink: "/img/simpsons.jpg",
+        imgLink: "/img/simpsons.webp",
         alt: "Desktop screenshot of cluedo simpsons project",
         title: "Simpsons Cluedo",
-        details: "Javascript HTML CSS.",
+        details: "TypeScript, Prettier, ESLint HTML, SCSS.",
         demoLink: "https://existenztim.github.io/Cluedo-with-localstorage-highscore/",
         id: "3"
     },
 
     {
-        imgLink: "/img/quant.jpg",
+        imgLink: "/img/greensock.webp",
         alt: "Desktop screenshot of firefly project",
-        title: "Simpsons Cluedo",
-        details: "Javascript HTML CSS.",
-        demoLink: "https://existenztim.github.io/Cluedo-with-localstorage-highscore/",
+        title: "SVG With GreenSock-Animation",
+        details: "JavaScript, HTML, GSAP.",
+        demoLink: "https://existenztim.github.io/SVG-with-GreenSock-Animation/",
         id: "4"
     }
 ];
 const Portfolio = () => {
   return (
-    <section className="bg-gray-200">
+    <section className="bg-gray-200 pb-8">
         <div id="projects" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
             <h3 className="text-4xl font-bold text-center text-gray-900 pb-16">Projects</h3>
             <p className="text-center py-8">
@@ -64,8 +64,10 @@ const Portfolio = () => {
                         <h3 className="text-2xl font-bold text-white tracking-wider text-center">
                             {project.title}
                         </h3>
-                        <p className="pb-4 bp-2 text-white text-center">{project.details}</p>
-                        <a href={project.demoLink}>
+                        <p className="pb-4 bp-2 text-white text-center">
+                            {project.details}
+                        </p>
+                        <a href={project.demoLink}  target="_blank" rel="noopener noreferrer">
                             <p className="text-center p-3 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-lg">Watch demo</p>
                         </a>
                       
