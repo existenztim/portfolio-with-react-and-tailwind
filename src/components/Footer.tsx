e3894e1ada7492a8eaa022b8df8efc6334f6198a
existenztim <tim920129@hotmail.com>
2023-02-07 22:45:42 +0100

    :tada: Dark mode + more accessibility

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,33 +4,33 @@ import { FaGithub, FaLinkedin, FaMapMarkerAlt } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <section className="bg-gray-300" id="get-in-touch">
+    <section className="bg-gray-300 dark:bg-black" id="get-in-touch">
        <div id="resume" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
-          <h3 className="text-4xl font-bold text-center text-gray-900 pb-16">Get in touch</h3>
+          <h3 className="text-4xl font-bold text-center text-gray-900 dark:text-gray-200 pb-16">Get in touch</h3>
             <div className="flex justify-center p-6">
               <a href="mailto:tim920129@hotmail.com" target="_blank" rel="noopener noreferrer"
                  aria-label="Write me an email"
                  className="px-6">
-                <AiFillMail className="cursor-pointer" size ={20}/>
+                <AiFillMail className="cursor-pointer dark:text-gray-200 hover:scale-125 ease-in duration-200" size ={20}/>
               </a>
               <a href="https://github.com/existenztim" target="_blank" rel="noopener noreferrer"
                  aria-label="Visit my github page"
                  className="px-6">
-                <FaGithub className="cursor-pointer" size ={20}/>
+                <FaGithub className="cursor-pointer dark:text-gray-200 hover:scale-125 ease-in duration-200" size ={20}/>
               </a>
               <a href="https://www.linkedin.com/in/tim-sundell-87b644252/" target="_blank" rel="noopener noreferrer"
                  aria-label="Visit my linkedIn page"
                  className="px-6">
-                <FaLinkedin className="cursor-pointer" size ={20}/>
+                <FaLinkedin className="cursor-pointer dark:text-gray-200 hover:scale-125 ease-in duration-200" size ={20}/>
               </a>
               <a href="https://goo.gl/maps/8sr45aLpEsRrRtyM7" target="_blank" rel="noopener noreferrer"
                  aria-label="Look where I live"
                  className="px-6">
-                <FaMapMarkerAlt className="cursor-pointer" size ={20}/>
+                <FaMapMarkerAlt className="cursor-pointer dark:text-gray-200 hover:scale-125 ease-in duration-200" size ={20}/>
                 
               </a>
             </div>
-            <p className="text-center text-lg font-semibold text-slate-600">tim920129@hotmail.com</p>
+            <p className="text-center text-lg font-semibold text-slate-600 dark:text-slate-300">tim920129@hotmail.com</p>
         </div>
   </section>
   )
